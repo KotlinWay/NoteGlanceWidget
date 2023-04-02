b494fa97d5b9ff36c6a78300d443061aff21fbd5
pikselnsk <pikselnsk@gmail.com>
2023-04-02 12:39:35 +0700

    added automatic widget update

--- app/src/main/java/com/example/note_glance_widget/note/NotesRepository.kt
+++ app/src/main/java/com/example/note_glance_widget/note/NotesRepository.kt
@@ -1,24 +1,40 @@
 package com.example.note_glance_widget.note
 
+import android.content.Context
+import androidx.datastore.preferences.core.Preferences
+import androidx.glance.appwidget.GlanceAppWidgetManager
+import androidx.glance.appwidget.state.updateAppWidgetState
+import androidx.glance.appwidget.updateIf
 import com.example.note_glance_widget.db.NoteDao
 import com.example.note_glance_widget.db.NoteDb
 import com.example.note_glance_widget.note.model.Note
 import com.example.note_glance_widget.note.model.toEntity
 import com.example.note_glance_widget.note.model.toEntityDb
+import com.example.note_glance_widget.widget.NoteWidget
+import com.example.note_glance_widget.widget.WidgetKeys.Prefs.noteIdPK
+import com.example.note_glance_widget.widget.WidgetKeys.Prefs.noteLastUpdatePK
+import com.example.note_glance_widget.widget.WidgetKeys.Prefs.noteTextPK
+import com.example.note_glance_widget.widget.WidgetKeys.Prefs.noteTitlePK
+import com.example.note_glance_widget.widget.mapNoteToWidget
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class NotesRepository @Inject constructor(
-    private val notesDao: NoteDao
+    private val notesDao: NoteDao,
+    @ApplicationContext private val context: Context
 ) {
 
     suspend fun addNewNote() = notesDao.insert(NoteDb())
 
     suspend fun getNote(noteId: Long) = notesDao.get(noteId)
 
-    suspend fun updateNote(note: Note) = notesDao.update(note.toEntityDb())
+    suspend fun updateNote(note: Note) {
+        notesDao.update(note.toEntityDb())
+        GlanceAppWidgetManager(context).mapNoteToWidget(context, note)
+    }
 
     fun getNotes() = notesDao.getListFlow().map { notesDb ->
         notesDb.map { note -> note.toEntity() }
--- app/src/main/java/com/example/note_glance_widget/widget/NoteWidget.kt
+++ app/src/main/java/com/example/note_glance_widget/widget/NoteWidget.kt
@@ -1,12 +1,17 @@
 package com.example.note_glance_widget.widget
 
+import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.datastore.preferences.core.Preferences
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetManager
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import androidx.glance.appwidget.state.updateAppWidgetState
+import androidx.glance.appwidget.updateIf
 import androidx.glance.currentState
 import androidx.glance.state.GlanceStateDefinition
 import androidx.glance.state.PreferencesGlanceStateDefinition
+import com.example.note_glance_widget.note.model.Note
 
 class NoteWidget : GlanceAppWidget() {
 
@@ -23,3 +28,16 @@ class NoteWidget : GlanceAppWidget() {
 class NoteWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = NoteWidget()
 }
+
+suspend fun GlanceAppWidgetManager.mapNoteToWidget(context: Context, note: Note) =
+    getGlanceIds(NoteWidget::class.java)
+        .forEach { glanceId ->
+            updateAppWidgetState(context, glanceId) { prefs ->
+                prefs[WidgetKeys.Prefs.noteTitlePK] = note.title
+                prefs[WidgetKeys.Prefs.noteTextPK] = note.text
+                prefs[WidgetKeys.Prefs.noteLastUpdatePK] = note.formatUpdatedAt
+            }
+            NoteWidget().updateIf<Preferences>(context) {
+                it[WidgetKeys.Prefs.noteIdPK] == note.id
+            }
+        }
